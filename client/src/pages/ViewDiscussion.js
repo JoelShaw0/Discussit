f7155c12b0e3eb29c826030b1114a9aca640c84d
JoelShaw0 <joelshaw@knights.ucf.edu>
2021-11-13 22:53:14 -0500

    Finishing Touches I guess
    If it works it works :100:

--- client/src/pages/ViewDiscussion.js
+++ client/src/pages/ViewDiscussion.js
@@ -13,9 +13,9 @@ export default function ViewDiscussion() {
                 <div className="flex fixed justify-start items-center flex-col h-4/6 right-20 bottom-16 bg-gray-100 rounded-2xl mt-20 p-12">
                     <p font-sourceSerifPro>User online</p>
                 </div>
-                <div className="flex fixed justify-start flex-col w-1/2 ml-16 h-1 right-72 bottom-16 bg-gray-100 rounded-2xl mt-20 p-6">
-                    <textarea className="" className="text-xl" type="text" placeholder="Start typing..."></textarea>
-                    <button className="right-72 h-15 w-20 text-black rounded-lg bg-gray-300 hover:bg-gray-200">Send</button>
+                <div className="flex fixed justify-start flex-row w-1/2 ml-16 h-1 right-72 bottom-16 bg-gray-100 rounded-2xl mt-20 p-6">
+                    <textarea className="p-5 px-20 text-black w-5/6 rounded-3xl text-xl" type="text" placeholder="Start typing..."></textarea>
+                    <button className="h-15 w-20 text-black rounded-lg bg-gray-300 hover:bg-gray-200">Send</button>
                 </div>
             </div>
         </div>
